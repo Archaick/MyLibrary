b5b65b82f0b54ae0208d11ef31926313601a8e15
Archaick <saeed.jh3@gmail.com>
2023-08-21 09:42:11 +0700

    initial files and layout

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MyLibrary
+
+Library site
+
+# Credits
+
+Hero background by klimkin at https://pixabay.com/videos/library-books-the-corridor-window-846/
---
+++ script.js
@@ -0,0 +1,4 @@
+// Select the video element
+const heroVideo = document.getElementById("hero-video");
+// Set the playback speed to a lower value (e.g., 0.5 for half speed)
+heroVideo.playbackRate = 0.7; // Adjust the value as needed
